a33c57649e33399cba5683d0ad8b80e2c76e689f
Esteban Flores <estflores1@gmail.com>
2018-07-17 20:01:44 -0600

    Initial Commit

---
+++ attempt1Tests/attempt1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  attempt1Tests.swift
+//  attempt1Tests
+//
+//  Created by Esteban Flores on 17/07/18.
+//  Copyright © 2018 Esteban Flores. All rights reserved.
+//
+
+import XCTest
+@testable import attempt1
+
+class attempt1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
